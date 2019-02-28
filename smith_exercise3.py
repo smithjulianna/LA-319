0df1d0072333ede97690744e7c2602d8f1e3995b
Julianna V. Smith <47000970+smithjulianna@users.noreply.github.com>
2019-02-28 09:54:47 -0600

    Add files via upload

---
+++ smith_exercise3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# cm to inch calculator
+inch = 2.54 #centimeters
+centimeters = int(input("Enter a number of centimeters"))
+print "Entered number of %s centimeters is equal to %s inches" % (centimeters , centimeters / inch)
+
